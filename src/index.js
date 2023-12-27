fe28e3c88f71a9366e2a47e17c87b562a3b91fb1
Georgi957 <gesh007@abv.bg>
2023-12-27 19:31:23 +0200

    Add home controller

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const expressConfigurator = require('./config/expressConfig');
 const handlebarsConfigurator = require('./config/handlebarsConfig');
+const routes = require('./router');
 
 const app = express();
 
@@ -9,10 +10,8 @@ const PORT = 5000;
 
 expressConfigurator(app);
 handlebarsConfigurator(app);
+app.use(routes);
 
-app.get('/', (req, res) => {
-    res.render('index');
-});
 
 app.get('/movies/add-movie', (req, res) => {
     res.render('add');
