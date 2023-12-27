82af0036304550eda067eac3cfaddb67bb00430c
Georgi957 <gesh007@abv.bg>
2023-12-27 19:36:55 +0200

    Add configuration for db

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 
 const expressConfigurator = require('./config/expressConfig');
 const handlebarsConfigurator = require('./config/handlebarsConfig');
+const dbConnect = require('./config/dbConfig');
 const routes = require('./router');
 
 const app = express();
@@ -10,6 +11,11 @@ const PORT = 5000;
 
 expressConfigurator(app);
 handlebarsConfigurator(app);
+
+dbConnect()
+    .then(() => console.log('Connected to database!'))
+    .catch((err) => console.error(err));
+
 app.use(routes);
 
 app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
