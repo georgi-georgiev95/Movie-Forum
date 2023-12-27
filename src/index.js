d331a95d8c917d23bbc37a7d7643822fefccd2fb
Georgi957 <gesh007@abv.bg>
2023-12-27 17:47:08 +0200

    Add handlebars and configurations for express and handlebars

--- src/index.js
+++ src/index.js
@@ -1,11 +1,17 @@
 const express = require('express');
 
+const expressConfigurator = require('./config/expressConfig');
+const handlebarsConfigurator = require('./config/handlebarsConfig');
+
 const app = express();
 
 const PORT = 5000;
 
+expressConfigurator(app);
+handlebarsConfigurator(app);
+
 app.get('/', (req, res) => {
-    res.send('Express integrated...');
+    res.render('index');
 })
 
 app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
