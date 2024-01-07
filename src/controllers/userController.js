27ffd04749ff22fbfdb85547f222dc5509d10949
Georgi957 <gesh007@abv.bg>
2024-01-07 18:18:51 +0200

    Add registered user to DB

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
+const userManager = require('../managers/userManager');
 
 router.get('/register', (req, res) => {
     res.render('users/register');
+});
+
+router.post('/register', async (req, res) => {
+    const { username, password, rePassword } = req.body;
+    await userManager.register({ username, password, rePassword });
+    res.redirect('/users/login');
 });
 
 router.get('/login', (req, res) => {
---
+++ src/models/User.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+userSchema.virtual('rePassword')
+    .set(function (value) {
+        if (value !== this.password) {
+            throw new mongoose.MongooseError('Password missmatch!')
+        }
+    });
+
+userSchema.pre('save', async function () {
+    const hash = await bcrypt.hash(this.password, 10);
+
+    this.password = hash;
+})
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
