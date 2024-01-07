502e6c3ad8ebc1e118b3a9dc839e8fcf5c90cf62
Georgi957 <gesh007@abv.bg>
2024-01-07 18:04:47 +0200

    Add register page

--- src/router.js
+++ src/router.js
@@ -2,8 +2,10 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/homeController');
 const movieController = require('./controllers/movieController');
+const userController = require('./controllers/userController');
 
 router.use(homeController);
 router.use(movieController);
+router.use('/users', userController);
 
 module.exports = router;
